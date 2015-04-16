a7e2cc8131cd5fd7546e7dc3488f366fcf4d575c
Dmytro (Mrjbee) Khudin <MisterJbee@gmail.com>
2015-04-17 02:07:01 +0300

    Improve script to run shutdown async

--- shutdown.py
+++ shutdown.py
@@ -17,7 +17,35 @@ def define(signature_builder):
 
 def execute(context, args_map, log):
     assert isinstance(context, command.ExecutionContext)
-    if 0 != os.system('shutdown '+args_map["TIME"]):
-        context.message("Shutdown fails", "Something goes wrong reboot fails")
+    time_arg = args_map["TIME"]
+    if time_arg == "now":
+        do_shutdown(time_arg, context)
+    else:
+        task = ShutdownTask(time_arg)
+        context.message("Shutdown scheduled", "Shutdown going to be executed later as accordingly to time argument ("+time_arg+")")
+        context.sub_task(task)
+
+
+def do_shutdown(time, context):
+    if 0 != os.system('shutdown '+time):
+        context.stop("Something goes wrong shutdown fails")
+        return False
     else:
         context.message("Shutdown...", "Going to shutdown computer")
+        return True
+
+
+class ShutdownTask (command.Task):
+
+    def __init__(self, time):
+        super(ShutdownTask, self).__init__()
+        self._time = time
+
+    def title(self):
+        return "Shutdown with "+self._time
+
+    def execute(self, context, log):
+        assert isinstance(context, command.TaskExecutionContext)
+        context.progress(0.2)
+        do_shutdown(self._time, context)
+        context.progress(1)
